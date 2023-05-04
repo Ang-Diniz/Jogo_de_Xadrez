2856449540331e3cc7ee5457e8a4e0eb671e21c3
Andre Gabriel <andregs.diniz@gmail.com>
2023-05-04 20:57:44 -0300

    Tratamento de erros com execção personalizada

--- Jogo_De_Xadrez_Console/Tabuleiro/Tabuleiro.cs
+++ Jogo_De_Xadrez_Console/Tabuleiro/Tabuleiro.cs
@@ -6,7 +6,7 @@
         public int colunas { get; set; }
         private Peca[,] pecas;
 
-        public Tabuleiro (int linha, int coluna)
+        public Tabuleiro(int linha, int coluna)
         {
             this.linhas = linha;
             this.colunas = coluna;
@@ -17,10 +17,41 @@
             return pecas[linha, coluna];
         }
 
-        public void colocarPeca(Peca p, Posicao pos)
+        public Peca peca(Posicao posicao)
         {
-            pecas[pos.linha, pos.coluna] = p;
-            p.posicao = pos;
+            return pecas[posicao.linha, posicao.coluna];
+        }
+
+        public bool existePeca (Posicao posicao)
+        {
+            validarPosicao(posicao);
+            return peca(posicao) != null;
+        }
+
+        public void colocarPeca(Peca p, Posicao posicao)
+        {
+            if(existePeca(posicao))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição");
+            }
+            pecas[posicao.linha, posicao.coluna] = p;
+            p.posicao = posicao;
+        }
+
+        public bool posicaoValida(Posicao posicao)
+        {
+            if (posicao.linha < 0 || posicao.linha >= linhas || posicao.coluna < 0 || posicao.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void validarPosicao(Posicao posicao)
+        {
+            if (!posicaoValida(posicao))
+            {
+                throw new TabuleiroException("Posição Inválida");
+            }
         }
     }
 }
